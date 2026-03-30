2b34c28222d2499b5c95cb6b1c1b40c8aaaae189
Manu <manmdyalw@gmail.com>
2026-03-30 10:01:23 +0200

    specialization between go-homes events

---
+++ src/main/java/be/technifutur/kinomichicommon/Constants.java
@@ -0,0 +1,10 @@
+package be.technifutur.kinomichicommon;
+
+public class Constants {
+    public static final int EXIT_CODE = -999;
+    public static final int BACK_CODE = -998;
+    public static final int BAIL_OUT_CODE = -997;
+    public static final int GO_HOME_CODE_CONDITIONED = -996;
+    public static final int GO_HOME_CODE_GRANTED = -995;
+    public static final int UNMATCH_CODE = -1;
+}
