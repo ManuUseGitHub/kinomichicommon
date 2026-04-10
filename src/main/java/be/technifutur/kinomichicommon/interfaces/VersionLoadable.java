ea093527ceeff00142f750f968eb5845c741040c
Manu <manmdyalw@gmail.com>
2026-04-10 16:09:25 +0200

    modify the version methods to include consumers call backs after operation to give more control

--- src/main/java/be/technifutur/kinomichicommon/interfaces/VersionLoadable.java
+++ src/main/java/be/technifutur/kinomichicommon/interfaces/VersionLoadable.java
@@ -1,8 +1,10 @@
 package be.technifutur.kinomichicommon.interfaces;
 
+import java.util.function.Consumer;
+
 public interface VersionLoadable<E> {
 
-    E load(String filename);
+    E load(String filename, Consumer<Boolean> cbComplete);
 
-    E loadByTextSource(String textContent);
+    E loadByTextSource(String textContent,Consumer<Boolean> cbComplete);
 }
--- src/main/java/be/technifutur/kinomichicommon/interfaces/VersionSavable.java
+++ src/main/java/be/technifutur/kinomichicommon/interfaces/VersionSavable.java
@@ -1,5 +1,7 @@
 package be.technifutur.kinomichicommon.interfaces;
 
+import java.util.function.Consumer;
+
 public interface VersionSavable<E> {
-    void save(E tts, String filename);
+    void save(E tts, String filename, Consumer<Boolean> cbComplete);
 }
